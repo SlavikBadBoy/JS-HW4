0b4de6dc646bec339a098576a7bbf42fcd51eb51
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-06-15 20:51:05 +0300

    fix js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 // task 1
 function checkTextFields() {
-  var field1Value = document.getElementById("field1").value;
-  var field2Value = document.getElementById("field2").value;
+  const field1Value = document.getElementById("field1").value;
+  const field2Value = document.getElementById("field2").value;
 
-  if (field1Value !== "" && field2Value !== "") {
+  if (field1Value && field2Value) {
     alert("Обидва поля заповнені");
   } else {
     alert("Не всі поля заповнені");
@@ -12,8 +12,8 @@ function checkTextFields() {
 }
 // task 2
 function checkNumberFields() {
-  var fieldValue1 = parseInt(document.getElementById("number1").value);
-  var fieldValue2 = parseInt(document.getElementById("number2").value);
+  const fieldValue1 = parseInt(document.getElementById("number1").value);
+  const fieldValue2 = parseInt(document.getElementById("number2").value);
 
   if (fieldValue1 + fieldValue2 > 10) {
     alert("Сума більша за 10");
@@ -23,7 +23,7 @@ function checkNumberFields() {
 }
 // task 3
 function checkForJavaScript() {
-  var textValue = document.getElementById("textField").value;
+  const textValue = document.getElementById("textField").value;
 
   if (textValue.includes("JavaScript")) {
     alert("Текст містить слово JavaScript");
@@ -33,9 +33,9 @@ function checkForJavaScript() {
 }
 // task 4
 function checkNumberRange() {
-  var fieldValue = parseInt(document.getElementById("numberField").value);
+  const fieldValue = parseInt(document.getElementById("numberField").value);
 
-  if (fieldValue > 10 && fieldValue < 20) {
+  if (fieldValue > 9 && fieldValue < 21) {
     alert("Число входить в діапазон від 10 до 20");
   } else {
     alert("Число не входить в діапазон від 10 до 20");
@@ -43,9 +43,9 @@ function checkNumberRange() {
 }
 // task 5
 function validateForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   if (
     name.length >= 3 &&
