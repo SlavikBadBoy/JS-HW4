ae30d44ce172c48fdb57715686468fa0305c8be4
SlavikBadBoy <buracenkoslavik@gmail.com>
2024-09-20 18:50:38 +0300

    Виправив помилки та переробив весь код

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 "use strict";
 // task 1
-function checkTextFields() {
-  const field1Value = document.getElementById("field1").value;
-  const field2Value = document.getElementById("field2").value;
 
-  if (field1Value && field2Value) {
+const checkButton = document.querySelector("#check-button");
+const field1Value = document.querySelector("#field1");
+const field2Value = document.querySelector("#field2");
+
+checkButton.addEventListener("click", () => {
+  if (field1Value.value && field2Value.value) {
     alert("Обидва поля заповнені");
   } else {
     alert("Не всі поля заповнені");
   }
-}
+});
 // task 2
-function checkNumberFields() {
-  const fieldValue1 = parseInt(document.getElementById("number1").value);
-  const fieldValue2 = parseInt(document.getElementById("number2").value);
+const number1 = document.querySelector("#number1");
+const number2 = document.querySelector("#number2");
+const checkSumButton = document.querySelector("#check-sum-button");
+
+checkSumButton.addEventListener("click", () => {
+  const fieldValue1 = parseInt(number1.value);
+  const fieldValue2 = parseInt(number2.value);
 
   if (fieldValue1 + fieldValue2 > 10) {
     alert("Сума більша за 10");
   } else {
     alert("Сума менша або дорівнює 10");
   }
-}
+});
 // task 3
-function checkForJavaScript() {
-  const textValue = document.getElementById("textField").value;
+const textField = document.querySelector("#textField");
+const checkTextButton = document.querySelector("#check-text-button");
+checkTextButton.addEventListener("click", () => {
+  const textValue = textField.value;
 
   if (textValue.includes("JavaScript")) {
     alert("Текст містить слово JavaScript");
   } else {
     alert("Текст не містить слово JavaScript");
   }
-}
+});
+
 // task 4
-function checkNumberRange() {
-  const fieldValue = parseInt(document.getElementById("numberField").value);
+const numberField = document.querySelector("#numberField");
+const checkRangeButton = document.querySelector("#check-range-button");
+
+checkRangeButton.addEventListener("click", () => {
+  const fieldValue = parseInt(numberField.value);
 
   if (fieldValue > 9 && fieldValue < 21) {
     alert("Число входить в діапазон від 10 до 20");
   } else {
     alert("Число не входить в діапазон від 10 до 20");
   }
-}
+});
+
 // task 5
-function validateForm() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const passwordField = document.querySelector("#password");
+const validateButton = document.querySelector("#validate-button");
+
+validateButton.addEventListener("click", () => {
+  const name = nameField.value;
+  const email = emailField.value;
+  const password = passwordField.value;
 
   if (
     name.length >= 3 &&
@@ -57,4 +75,4 @@ function validateForm() {
   } else {
     alert("Помилка! Перевірте правильність введених даних.");
   }
-}
+});
